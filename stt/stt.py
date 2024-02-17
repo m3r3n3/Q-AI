af28fbc51f0d482f90145c88a885caaf0b8a9798
mariasabi16 <mariasabi16@gmail.com>
2024-02-17 11:13:17 +0530

    Sentiment with text

--- stt/stt.py
+++ stt/stt.py
@@ -8,11 +8,11 @@ import speech_recognition as sr
 import os
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
-from fastpunct import FastPunct
-import shutil
+# from fastpunct import FastPunct
+#import shutil
 # create a speech recognition object
 r = sr.Recognizer()
-fastpunct = FastPunct()
+#fastpunct = FastPunct()
 sound = AudioSegment.from_mp3("transcribe3.mp3")
 sound.export("transcribe3.wav", format="wav")
 path = "transcribe3.wav"
@@ -25,7 +25,7 @@ def transcribe_audio(path):
         audio_listened = r.record(source)
         # try converting it to text
         text = r.recognize_google(audio_listened)
-        fastpunct.punct([text])
+       # fastpunct.punct([text])
     return text
 
 # a function that splits the audio file into chunks on silence
